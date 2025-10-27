02931ae7d5d06aab1307db8450ee19cdfe8f7eb3
Risoca Developer <risoca.developer@gmail.com>
2025-10-27 06:30:07 +0000

    14:22:55.482 Running build in Washington, D.C., USA (East) – iad1 14:22:

--- src/components/layout/user-nav.tsx
+++ src/components/layout/user-nav.tsx
@@ -23,7 +23,7 @@ import { cn } from "@/lib/utils";
 interface UserNavProps {}
 
 export function UserNav({}: UserNavProps) {
-  const { user, openAuthDialog, logout, isInitialLoading } = useAuth();
+  const { user, openAuthDialog, logout, isInitialLoading, refreshUser } = useAuth();
   const { toast } = useToast();
   const [hasMounted, setHasMounted] = useState(false);
 
@@ -104,7 +104,7 @@ export function UserNav({}: UserNavProps) {
               )}
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            <DropdownMenuItem onClick={logout}>
+            <DropdownMenuItem onClick={() => logout()}>
               <LogOut className="mr-2 h-4 w-4" />
               <span>Log out</span>
             </DropdownMenuItem>
