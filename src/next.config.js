548850171f08b07888e41f1191068dd314f683d2
Risoca Developer <risoca.developer@gmail.com>
2025-10-29 15:07:41 +0000

    for PWA

--- src/next.config.js
+++ src/next.config.js
@@ -6,6 +6,18 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offlineCache',
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
 });
 
 const nextConfig = {
